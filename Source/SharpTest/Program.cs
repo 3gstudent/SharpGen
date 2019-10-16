c2de8dda7ac0cf2759e6a1e38c044210c46b26a6
3gstudent <15814408+3gstudent@users.noreply.github.com>
2019-10-16 00:43:40 -0700

    Create Program.cs

---
+++ Source/SharpTest/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Management;
+
+namespace SharpTest
+{
+    class Program
+    {
+        public static void TestMethod(string string1)
+        {
+            Console.WriteLine(string1);
+        }
+    }
+}
